0dae6cf8643c10d0c9032a674c64c66ff76aff19
kevin <kevinsequeira23@gmail.com>
2019-10-24 10:23:13 +0200

    inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EvaluacionComponent } from './evaluacion/evaluacion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EvaluacionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
